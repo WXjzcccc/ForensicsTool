323472a7383acc4dc2360fa2efffae3ed8e4bb63
WXjzcccc <wxjzcroot@gmail.com>
2025-12-02 13:57:29 +0800

    页面切换时增加过渡动画

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -34,9 +34,38 @@ onUnmounted(() => {
         <div class="app-body">
             <AppSidebar />
             <div class="app-content">
-                <router-view />
+                <transition name="page-transition" mode="out-in">
+                    <router-view />
+                </transition>
             </div>
         </div>
         <!-- <AppFooter /> -->
     </div>
 </template>
+
+<style>
+/* 页面过渡效果样式 */
+.page-transition-enter-active,
+.page-transition-leave-active {
+    transition: all 400ms ease;
+}
+
+.page-transition-enter-from {
+    transform: scale(0.9);
+}
+
+.page-transition-leave-to {
+    transform: scale(1.1);
+}
+
+.page-transition-enter-active,
+.page-transition-leave-active {
+    transition: transform 400ms ease, opacity 400ms ease;
+}
+
+.page-transition-enter-from,
+.page-transition-leave-to {
+    opacity: 0;
+}
+
+</style>
